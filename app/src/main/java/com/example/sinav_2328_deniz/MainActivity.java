0c8f4f316d0363bfb2dd422d972314552f7e2bab
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-05-24 09:45:22 +0300

    no message

---
+++ app/src/main/java/com/example/sinav_2328_deniz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sinav_2328_deniz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
